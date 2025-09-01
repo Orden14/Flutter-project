30a0c543022d185436328cc182ad98081e45fffd
Orden14 <orden0614@gmail.com>
2025-09-01 12:29:32 +0200

    add basic auth system with firebase

---
+++ lib/home/welcome_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Welcome',
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+      ),
+    );
+  }
+}
